a40889f072a02399c03694a57533af6ced64c961
Reema Dhanwani <reemadhanwani55@gmail.com>
2026-07-12 12:29:13 -0700

    Added redis package and calling redis method

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const ApiRoutes = require('./src/routes/index');
-const {PORT} = require('./src/config/envConfig');
+const { PORT } = require('./src/config/envConfig');
+const { connectRedis } = require("./src/config/RedisConfig");
 
 
 const setupAndStartServer = async () => {
     const app = express();
-
+    await connectRedis();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
